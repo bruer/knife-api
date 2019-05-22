8953bc00a3c8f403ef52f2470cd0f48b87d9ded7
bruer <f.bruer@gmail.com>
2019-05-22 11:41:16 +0200

    la till routes/metoder för att lägga till kommentarer och att posta en kommentar

--- src/classes/Comment.php
+++ src/classes/Comment.php
@@ -1,9 +1,25 @@
 <?php
 
 class Comment extends Mapper {
-  public function getComments() {
+    
+    public function getAll() {
     $statement = $this->db->prepare("SELECT * FROM comments");
     $statement->execute();
+    
     return $statement->fetchAll(PDO::FETCH_ASSOC);
-  }
+    }
+    
+    public function post($entryID, $content, $createdBy, $createdAt) {
+        $statement = $this->db->prepare(
+            "INSERT INTO comments (entryID, content, createdBy, createdAt)
+            VALUES (:entryID, :content, :createdBy, :createdAt)"
+        );
+
+        $statement->execute([
+            ':entryID' => $entryID,
+            ':content' => $content,
+            ':createdBy' => $createdBy,
+            ':createdAt' => $createdAt
+        ]);
+    }
 }
--- src/routes/comments.php
+++ src/routes/comments.php
@@ -4,9 +4,21 @@ return function ($app) {
   // Register auth middleware
   $auth = require __DIR__ . '/../middlewares/auth.php';
 
-  $app->get('/api/comments', function ($request, $response) {
+  $app->get('/api/comments', function($request, $response) {
     $comment = new Comment($this->db);
 
-    return $response->withJson($comment->getComments());
+    return $response->withJson($comment->getAll());
+  });
+
+  $app->post('/api/comment', function($request, $response) {
+    $comment = new Comment($this->db);
+
+    $data = $request->getParsedBody();
+
+    $response->withJson($comment->post(
+      $data['entryID'], $data['content'], $data['createdBy'], $data['createdAt']
+    ));
+    
+    return $response->withJson($data);
   });
 };
