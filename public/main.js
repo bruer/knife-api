5cdd22a3fc8007a0a9c595fed20d2a6ca0a04e82
emmitorstensson <emmitorstensson@hotmail.com>
2019-05-22 11:44:00 +0200

    Registrera ny anvndare

--- public/main.js
+++ public/main.js
@@ -1,3 +1,57 @@
-(function(){
-  console.log('Hello World!');
-})();
+
+
+const views = {
+    login: ['#loginFormTemplate'], 
+    register: ['#registerFormTemplate']
+  }
+  
+  function renderView(view) {
+    // 1. Definera ett target
+    const target = document.querySelector('main');
+  
+    // 2 Loopa igenom vår "view"
+    view.forEach(template => {
+      // 3. hämta innehållet i tampleten
+      const templateMarkup = document.querySelector(template).innerHTML;
+      
+      // 4. Skapa en div 
+      const div = document.createElement('div');
+      
+      // 4.2 Fyll diven med innehåll
+      div.innerHTML = templateMarkup;
+      
+      // 5. Lägg in diven i target
+      target.append(div)
+    });
+  }
+    renderView(views.register)
+  
+  const registerForm = document.querySelector('#RegisterForm')
+  registerForm.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    const formData = new FormData(registerForm)
+    fetch('/api/register', {
+
+      method: 'POST',
+      body: formData
+
+    }).then(response => {
+        
+      if(!response.ok){
+        return Error(response.statusText)
+      } else {
+        return response.json()
+        renderView(view.loggedIn) // OBS GÖR SJÄLV
+      }
+    }).catch(error => {
+      console.error(error)
+    })
+  })
+  
+  
+  fetch ('/api/users')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
