6c8ec34a20d7748da1328947900215c6292cde25
emmitorstensson <emmitorstensson@hotmail.com>
2019-05-22 13:36:38 +0200

    fixat reg-form v1

--- src/routes/user.php
+++ src/routes/user.php
@@ -12,6 +12,7 @@ return function ($app) {
     return $response->withJson($user->getUserByID($userID));
   })->add($auth);
 
+  // 
   $app->post('/api/user', function ($request, $response) {
      $user = new User($this->db);
      $data = $request->getParsedBody();
