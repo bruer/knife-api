e2af998d28030f91686e24c0f8c70130a380b2d0
karber <karl.berglof@hotmail.com>
2019-05-22 12:50:26 +0200

    correct changes

--- src/classes/Entry.php
+++ src/classes/Entry.php
@@ -39,44 +39,6 @@ class Entry extends Mapper {
       return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-/*     public function getLatestPosts($quantity) {
-
-        $statement = $this->db->prepare("SELECT * FROM entries ORDER BY createdAt DESC LIMIT {$quantity}");
-        $statement->execute();
-
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function getFirstPosts($quantity){
-
-        $statement = $this->db->prepare("SELECT * FROM entries ORDER BY createdAt ASC LIMIT {$quantity}");
-        $statement->execute();
-
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function getEntriesByUserID($user){
-
-      $statement = $this->db->prepare("SELECT * FROM entries WHERE userID = {$user}");
-      $statement->execute();
-
-      return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function getEntriesFrom($user, $quantity){
-
-      $statement = $this->db->prepare("SELECT * FROM entries WHERE userID = {$user} ORDER BY createdAt DESC LIMIT {$quantity}");
-      $statement->execute();
-
-      return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function firstEntriesFrom($user, $quantity){
-      $statement = $this->db->prepare("SELECT * FROM entries WHERE userID = {$user} ORDER BY createdAt ASC LIMIT {$quantity}");
-      $statement->execute();
-
-      return $statement->fetchAll(PDO::FETCH_ASSOC);
-    } */
   }
 
 ?>
