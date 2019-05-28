0d7e8e0b83dda14772037e3a685cd9d0589e7451
karber <karl.berglof@hotmail.com>
2019-05-28 15:28:33 +0200

    removed unused code

--- public/main.js
+++ public/main.js
@@ -249,17 +249,6 @@ function showAllEntries() {
                     </div>
                 </div>
               <div class="row">
-                <div class="col d-flex justify-content-center">
-                  <form id="updateEntryForm${id}" class="hidden">
-                    <div class="form-group">
-                    <input type="text" class="form-control" placeholder="New content here..." name="content"> 
-                    </div>
-                    <button type="submit" class="btn btn-success btn-sm">Post edit</button>  
-                    <a href="javascript:hideUpdateForm(${id})" class="btn btn-secondary btn-sm">Cancel</a>     
-                  </form>
-                </div>
-              </div>
-              <div class="row">
                   <div class="col d-flex justify-content-center">
                       <p class="text-muted">${entry.createdAt}</p>
                   </div>
@@ -298,11 +287,6 @@ function showAllEntries() {
 
     // Hämta entryID för varje inlägg
     entries.forEach(entry => {
-      // radera entries
-      deleteEntry(entry.entryID);
-
-      //updatera entries
-      updateEntry(entry.entryID);
 
       // Visa inläggets kommentarer
       showComments(entry.entryID);
